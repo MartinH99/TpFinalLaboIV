ed58a96bc3fd3f6163b3fedc17361f407df21374
@mlabatut <mlabatut@makerelectronica.com.ar>
2024-10-23 11:58:11 -0300

    24.1023 ADD -> Carpeta -pages- para dividir la interfaz grafica del proyecto ADD -> Carpeta -components- para reutilizar componentes en todo el proyecto MOD -> Arquitectura de carpetas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    ProfilePageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
