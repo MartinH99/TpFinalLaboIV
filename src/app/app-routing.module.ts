aef2d34e521d6531f8acf72d16cb7ce4e639bf08
Mariano Labatut <marianolabatut@gmail.com>
2024-10-26 22:47:16 -0300

    24.1026 ADD -> Solucionando problemas de conflictos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,16 @@
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { LoginComponent } from './pages/login-page/login-page.component';
 
 const routes: Routes = [
-<<<<<<< HEAD
-  {path:'register',component:RegisterPageComponent}
-=======
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterPageComponent },
   { path: 'profile', component: ProfilePageComponent },
   { path: 'home', component: MainPageComponent },
   { path: '**', redirectTo: 'login' }
->>>>>>> e52d8752473feb3117b70795d1c43613c5e0d076
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-=======
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
->>>>>>> e52d8752473feb3117b70795d1c43613c5e0d076
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
@@ -17,15 +14,11 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-<<<<<<< HEAD
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-=======
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { MusicPlaybackComponent } from './components/music-playback/music-playback.component';
->>>>>>> e52d8752473feb3117b70795d1c43613c5e0d076
 
 @NgModule({
   declarations: [
@@ -44,15 +37,11 @@ import { MusicPlaybackComponent } from './components/music-playback/music-playba
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-<<<<<<< HEAD
-    ReactiveFormsModule
-=======
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatIconModule
->>>>>>> e52d8752473feb3117b70795d1c43613c5e0d076
   ],
   providers: [],
   bootstrap: [AppComponent]
