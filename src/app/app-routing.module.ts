e52d8752473feb3117b70795d1c43613c5e0d076
Mariano Labatut <marianolabatut@gmail.com>
2024-10-26 21:02:20 -0300

    24.1026 FIX -> Error en app-routing-module en la ruta  LoginPageComponent, no lo encontraba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { LoginComponent } from './pages/login-page/login-page.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginPageComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterPageComponent },
   { path: 'profile', component: ProfilePageComponent },
   { path: 'home', component: MainPageComponent },
